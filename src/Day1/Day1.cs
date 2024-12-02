54118ed16b88be984064c0e37dacb806ca6114a6
GeniusOctopus <k.white221@outlook.com>
2024-12-02 21:49:04 +0100

    feat: Day 1

---
+++ src/Day1/Day1.cs
@@ -0,0 +1,41 @@
+﻿namespace AoC2024.Day1
+{
+    internal class Day1
+    {
+        public void Solve()
+        {
+            var input = File.ReadAllLines("Day1/input.txt");
+            var distance = 0;
+            var groupDistance = 0;
+
+            List<int> leftList = [];
+            List<int> rightList = [];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var distances = input[i].Split("   ");
+                leftList.Add(int.Parse(distances[0]));
+                rightList.Add(int.Parse(distances[1]));
+            }
+
+            leftList.Sort();
+            rightList.Sort();
+            var groups = rightList.GroupBy(x => x);
+
+            for (int i = 0; i < leftList.Count; i++)
+            {
+                distance += Math.Abs(leftList[i] - rightList[i]);
+
+                var group = groups.FirstOrDefault(x => x.Key == leftList[i]);
+
+                if (group != null)
+                {
+                    groupDistance += leftList[i] * group.Count();
+                }
+            }
+
+            Console.WriteLine(distance);
+            Console.WriteLine(groupDistance);
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AoC2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Day1.Day1().Solve();
+        }
+    }
+}
